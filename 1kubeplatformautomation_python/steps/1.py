42311b9861da8d388c3d25403140fcf62279b6ed
Gebru Abreha Woldeyohannes <gebru.abreha.woldeyohannes@ericsson.com>
2026-05-21 16:51:25 +0200

    firstcommit

--- 1kubeplatformautomation_python/steps/1.py
+++ 1kubeplatformautomation_python/steps/1.py
@@ -4,3 +4,34 @@ import subprocess
 import logging
 import shutil
 
+#loggging
+
+#path to different folder and directorys
+
+# Setup dependencies
+
+# Setup tools
+
+# Shell detection (robust)
+
+# Infra execution
+
+# Test execution
+
+# Main orchestration or clal methods:
+# ----------------------------
+def run(feature_file="features/health_check.feature"):
+    log.info("=== START LOCAL ORCHESTRATION ===")
+
+    try:
+        install_dependencies()
+
+        run_infra("setup")
+        run_tests(feature_file)
+
+        log.info("SUCCESS")
+
+    finally:
+        run_infra("teardown")
+
+# CLI entrypoint
